380c72ac0fcce7f1be114c29953d4868effe0a34
s033Fish <s12105fish@outlook.com>
2024-12-24 14:44:25 -0500

    Adding static folder

--- webapps/settings.py
+++ webapps/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.133.135.4']
 
 
 # Application definition
