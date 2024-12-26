610c946330d3da36aafd2ef41236bbe9fa11c199
s033Fish <s12105fish@outlook.com>
2024-12-26 17:23:22 -0500

    added new host

--- webapps/settings.py
+++ webapps/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.133.135.4']
+ALLOWED_HOSTS = ['3.133.135.4', '127.0.0.1', "phideltchores.com", "www.phideltchores.com"]
 
 
 # Application definition
